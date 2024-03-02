d1051c602d1ed2d713f31f24773ca1b00453a915
limunozca <160246649+limunozca@users.noreply.github.com>
2024-03-01 20:08:36 -0500

    Add files via upload

--- word_count.py
+++ word_count.py
@@ -1,74 +1,50 @@
-"""Taller evaluable""" 
+"""Taller evaluable"""
 
 import glob
 
 import pandas as pd
 
-
 def load_input(input_directory):
     """Load text files in 'input_directory/'"""
-    #
-    # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
-    # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
-    # entrada en el DataFrame.
-    #
-
-    filenames = glob.glob(f"{input_directory}/*.txt")
-
-    #dataframes = []
-    #for filename in filenames:
-        #dataframes.append(pd.read_csv(filename, sep="\t", header=None, names=["text"]))
-
-    dataframes =[
+    filenames =glob.glob(f"{input_directory}/*.txt")
+    
+    dataframes =  [
         pd.read_csv(filename, sep="\t", header=None, names=["text"])
         for filename in filenames
-    ]
-
+    ]  
     concatenated_df = pd.concat(dataframes, ignore_index=True)
     return concatenated_df
 
 
-   
 def clean_text(dataframe):
     """Text cleaning"""
-    #
-    # Elimine la puntuación y convierta el texto a minúsculas.
-    #
-
-    dataframe = dataframe.copy()
-    dataframe["text"] = dataframe["text"].str.lower()
-    dataframe["text"] = dataframe["text"].str.replace(".", "")
-    dataframe["text"] = dataframe["text"].str.replace(",", "")
+    
+    dataframe=dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.lower() #poner todas las palabras en minuscula
+    dataframe["text"] = dataframe["text"].str.replace(".","") #reemplazar .
+    dataframe["text"] = dataframe["text"].str.replace(",","") #reemplazar ,
     return dataframe
+
 
 def count_words_(dataframe):
     """Word count"""
-    dataframe =dataframe.copy()
-    dataframe["text"] = dataframe["text"].str.split()
-    dataframe = dataframe.explode("text")
+    dataframe=dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split() #se crea una lista en cada fila con separacion por ,
+    dataframe = dataframe.explode("text") 
     dataframe["count"] = 1
-    dataframe = dataframe.groupby("text").agg({"count": "sum"})
+    dataframe = dataframe.groupby("text").agg({"count": "sum"}) #sumar la columna count
     return dataframe
-    #
-
 
 def count_words(dataframe):
     """Word count"""
-    dataframe = dataframe.copy()
-    dataframe["text"] = dataframe["text"].str.split()
-    dataframe = dataframe.explode("text")
-    dataframe = dataframe["text"].value_counts()
-    #dataframe["count"] = 1
-
-    #dataframe = dataframe.groupby("text", as_index=False).agg({"count": "sum"})
-    
+    dataframe=dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split() 
+    dataframe = dataframe.explode("text") 
+    dataframe =dataframe["text"].value_counts()
     return dataframe
-
-
-
 def save_output(dataframe, output_filename):
     """Save output to a file."""
-    dataframe.to_csv(output_filename, sep="\t", index=True)
+    dataframe.to_csv(output_filename, sep=";", index=True)
 
 def run(input_directory, output_filename):
     """Call all functions."""
@@ -82,4 +58,4 @@ if __name__ == "__main__":
     run(
         "input",
         "output.txt",
-    )
+    )  
